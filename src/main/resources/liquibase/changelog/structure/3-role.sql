45300fb05a58eb415c85eca3b2e219447933ffb3
Sergey Didenko <sergey.didenko.dev@gmail.com>
2019-02-04 23:24:53 +0200

    [F-2] JWT Authorization :  * Fixed db seeds for auth

--- src/main/resources/liquibase/changelog/structure/3-role.sql
+++ src/main/resources/liquibase/changelog/structure/3-role.sql
@@ -3,7 +3,7 @@
 --changeset Sergey-Didenko:3
 CREATE TABLE `role` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `name` varchar(60) DEFAULT NULL,
+  `role` varchar(60) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `UK_nb4h0p6txrmfc0xbrd1kglp9t` (`name`)
 ) ENGINE=InnoDB AUTO_INCREMENT=0 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
